754e19e645b7d7fb96eb82bb09c17bdfad6216b0
leafdesk <lsg001008@gmail.com>
2023-01-16 18:06:20 +0900

    :hammer: 스크립트 추가 - IonNav를 사용한 RootPage 구성.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { IonNav } from '@ionic/react';
+
 export default function Home() {
+  const [rootPage, setRootPage] = useState<Function>();
+
+  useEffect(() => {
+    setRootPage(() => RootPage);
+  }, []);
+
+  return <IonNav root={rootPage} />;
+}
+
+function RootPage() {
   return;
 }
